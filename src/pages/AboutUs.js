ce3c225739385867ae45a7f899fa8a7258f69d00
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-01-11 18:37:50 -0800

    About Us Section/Page Add

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = props => {
+  return (
+    <div className="AboutUs">
+      <AboutSection />
+    </div>
+  );
+};
+
+export default AboutUs;
